6115c29007e5999613009c23085125a91c231b6d
Usama Hamid <usamahamid007@gmail.com>
2019-01-22 15:37:40 +0530

    Introduced Day3 problem

---
+++ solutions/day3/solution.go
@@ -0,0 +1,18 @@
+package main
+
+type Node struct {
+	val   string
+	left  Node
+	right Node
+}
+
+func main() {
+}
+
+func serializeTreeToString(node Node) string {
+	return ""
+}
+
+func deserializeStringToTree(serializedString string) Node {
+	return Node{}
+}
