2d10e900688db700cd7f1c9eb8b3758016ca0a63
Usama Hamid <usamahamid007@gmail.com>
2019-01-22 15:54:03 +0530

    Updated Day1 solution

--- solutions/day1/solution.go
+++ solutions/day1/solution.go
@@ -4,13 +4,12 @@ import "fmt"
 
 func main() {
 	array := []int{10, 15, 3, 7}
-	fmt.Println(checkIfElementPairCanAddTo(array, len(array), 10))
+	fmt.Println(checkIfElementPairCanAddTo(array, 11))
 }
 
-func checkIfElementPairCanAddTo(array []int, length int, sum int) bool {
+func checkIfElementPairCanAddTo(array []int, sum int) bool {
 	existingItemsMap := make(map[int]int)
-	for i := 0; i < length; i++ {
-		ithElement := array[i]
+	for i, ithElement := range array {
 		_, isAvailable := existingItemsMap[sum-ithElement]
 		if isAvailable {
 			return true
