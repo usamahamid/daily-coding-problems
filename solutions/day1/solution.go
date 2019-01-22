2f3575956cfd19c1649f2eda446d4d4ac8f55d0f
Usama Hamid <usamahamid007@gmail.com>
2019-01-22 14:09:31 +0530

    Minor Formatting Change

--- solutions/day1/solution.go
+++ solutions/day1/solution.go
@@ -7,12 +7,11 @@ func main() {
 	fmt.Println(checkIfElementPairCanAddTo(array, len(array), 10))
 }
 
-
 func checkIfElementPairCanAddTo(array []int, length int, sum int) bool {
 	existingItemsMap := make(map[int]int)
 	for i := 0; i < length; i++ {
 		ithElement := array[i]
-		_, isAvailable := existingItemsMap[sum - ithElement]
+		_, isAvailable := existingItemsMap[sum-ithElement]
 		if isAvailable {
 			return true
 		}
