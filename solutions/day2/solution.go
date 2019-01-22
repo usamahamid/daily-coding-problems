a1552b1db434c265d552cc92cb7a82d44a789cd6
Usama Hamid <usamahamid007@gmail.com>
2019-01-22 15:12:16 +0530

    Introduced Day2 Solution

--- solutions/day2/solution.go
+++ solutions/day2/solution.go
@@ -1,4 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+	array := []int{1, 2, 3, 4, 5}
+	fmt.Println(getProductArray(array, len(array)))
+}
+
+func getProductArray(array []int, length int) []int {
+	var result = make([]int, length)
+	temp := 1
+	for i := 0; i < length; i++ {
+		result[i] = temp
+		temp *= array[i]
+	}
+
+	temp = 1
+	for i := length - 1; i >= 0; i-- {
+		result[i] *= temp
+		temp *= array[i]
+	}
+	return result
 }
