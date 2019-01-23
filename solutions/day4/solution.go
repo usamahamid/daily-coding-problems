9484ecae8c695d20d611325814ad31b0c23c648e
Usama Hamid <usamahamid007@gmail.com>
2019-01-23 12:33:27 +0530

    Introduced Day4 solution skeleton

---
+++ solutions/day4/solution.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	array := []int{3, 4, -1, 1}
+	fmt.Println(getLowestMissingPositiveNumberInArray(array, len(array)))
+}
+
+func getLowestMissingPositiveNumberInArray(array []int, length int) int {
+	return 0
+}
