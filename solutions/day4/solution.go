d8cb9253d18fd22c598eaec58e991c597273b70f
Usama Hamid <usamahamid007@gmail.com>
2019-01-28 16:51:03 +0530

    Introduced Day4 solution

--- solutions/day4/solution.go
+++ solutions/day4/solution.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	array := []int{3, 4, -1, 1}
+	array := []int{3, 4, -1, 1, 2}
 	fmt.Println(getLowestMissingPositiveNumberInArray(array, len(array)))
 }
 
-func getLowestMissingPositiveNumberInArray(array []int, length int) int {
-	return 0
+func getLowestMissingPositiveNumberInArray(array []int, length int) (bool, int) {
+	var result = make([]int, length)
+	for _, ithElement := range array {
+		if ithElement > 0 && ithElement < length {
+			result[ithElement-1] = 1
+		}
+	}
+
+	fmt.Println(result)
+	for i, ithElement := range result {
+		if ithElement != 1 {
+			return true, i + 1
+		}
+	}
+	return false, -1
 }
