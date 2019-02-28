95d4b74f6fb38daddc19267a6b40ea1318566a90
Usama Hamid <usamahamid.amjadali@impigertech.com>
2019-02-28 22:58:36 +0530

    Introduced Solution for Problem#9

--- solutions/Problem#0009/solution.go
+++ solutions/Problem#0009/solution.go
@@ -13,6 +13,18 @@ func getMaximumSumOfNonAdjacentElements(array []int, len int) int {
 	if len == 0 {
 		return 0
 	}
+	if len == 1 {
+		return array[0]
+	}
+	incl := array[0]
+	excl := 0
+	newExcl := 0
+	for i := 1; i < len; i++ {
+		newExcl = max(incl, excl)
+		incl = excl + array[i]
+		excl = newExcl
+	}
+	return max(incl, excl)
 }
 
 func max(a, b int) int {
