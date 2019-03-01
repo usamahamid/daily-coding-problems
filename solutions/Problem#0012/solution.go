01276dfd3b88c0bdb30985a4e42f739753b5a5ca
Usama Hamid <usamahamid007@gmail.com>
2019-03-01 11:06:31 +0530

    Introduced Solution for Problem#12

--- solutions/Problem#0012/solution.go
+++ solutions/Problem#0012/solution.go
@@ -1,12 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	array := []int{1, 2}
-	fmt.Println(getCount(5, array, len(array)))
+	fmt.Println(getCountUtil(4, array))
+}
+func getCountUtil(n int, array []int) int {
+	sort.Ints(array)
+	return getCount(n, array)
 }
 
-func getCount(n int, array []int, length int) int {
-
+func getCount(n int, array []int) int {
+	if n == 0 {
+		return 1
+	}
+	count := 0
+	for _, ithElement := range array {
+		if ithElement <= n {
+			count += getCount(n-ithElement, array)
+		} else {
+			break
+		}
+	}
+	return count
 }
