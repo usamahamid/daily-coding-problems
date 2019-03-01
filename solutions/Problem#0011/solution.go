4c1dc58a0536cb165ffbc3213d63c3fa3b4c16ea
Usama Hamid <usamahamid007@gmail.com>
2019-03-01 10:46:22 +0530

    Partial solution for Problem#11

--- solutions/Problem#0011/solution.go
+++ solutions/Problem#0011/solution.go
@@ -1,4 +1,11 @@
 package main
 
+import Trie from "./trie"
+
 func main() {
+	array := []string{"dog", "deer", "deal"}
+	dictTrie := buildDictionary(array)
+}
+func buildDictionary(array []string) *Trie {
+	return trie.New()
 }
